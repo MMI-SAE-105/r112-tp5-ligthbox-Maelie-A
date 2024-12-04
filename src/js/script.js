3b87bc8935857fddf4aa639390f2c437a6a4c1df
MaëlieA <maelie@addario.fr>
2024-12-04 08:32:54 +0100

    Ajout de la gestion de la lightbox : sélection des éléments, détection des clics et mise à jour de l'image

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,21 @@ const lightBox = document.querySelector("#lightbox");
 const image = document.querySelector("#lightbox img");
 
 
+// Surveille les clics sur toute la page
+document.addEventListener("click", function (e) {
+  // Vérifie si l'élément cliqué a l'attribut "data-full-img" ou si la lightbox est ouverte
+  if (e.target.matches("[data-full-img]") || lightBox.open) {
+    // Ferme la lightbox si elle est ouverte
+    if (lightBox.open) {
+      lightBox.close();
+    } else {
+      // Sinon, ouvre la lightbox
+      lightBox.showModal();
+    }
+    image.src = e.target.dataset.fullImg;
+  }
+});
+
+
+
 
