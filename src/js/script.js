4d2671620b91aafa3f331ff19f26efa76742c7c5
MaëlieA <maelie@addario.fr>
2024-12-04 08:25:22 +0100

    Lightbox et img

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 // *** LIGHTBOX ***
+// Choisis l'élément avec l'ID "lightbox"
+const lightBox = document.querySelector("#lightbox");
+
+// Prend l'image à l'intérieur de "lightbox"
+const image = document.querySelector("#lightbox img");
+
+
+
